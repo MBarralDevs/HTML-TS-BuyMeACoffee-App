d47ace7eb0a275802aadc775b9a6098457d13aa3
mbarraldevs <mbarraldevs@outlook.com>
2025-10-21 12:04:53 +0200

    upgrade

--- index-js.js
+++ index-js.js
@@ -2,7 +2,10 @@ import {
   createWalletClient,
   custom,
   createPublicClient,
+  parseEther,
+  defineChain,
 } from "https://esm.sh/viem";
+import { contractAddress, abi } from "./constants-js";
 
 const connectBtn = document.getElementById("connectBtn");
 const buyCoffeeBtn = document.getElementById("buyCoffeeBtn");
@@ -46,17 +49,22 @@ async function buyCoffee() {
     transport: custom(window.ethereum),
   });
 
-  await walletClient.requestAddresses();
+  const [connectedAccounts] = await walletClient.requestAddresses();
 
   // Initialize publicClient
   publicClient = createPublicClient({
     transport: custom(window.ethereum),
   });
 
-  //
-  /*const {request} = await publicClient.simulateTransaction({
-    address:
-})*/
+  //We are simulating the transaction here, to test it before sending it for real
+  const { request } = await publicClient.simulateTransaction({
+    address: contractAddress,
+    abi: abi,
+    functionName: "fund",
+    account: connectedAccounts,
+    chain: await getCurrentChain(walletClient),
+    amount: parseEther(ethAmount),
+  });
 }
 
 function getBalance() {
@@ -75,6 +83,25 @@ function withdraw() {
   }
   console.log("Withdrawing funds...");
   // Add withdraw logic here
+}
+
+async function getCurrentChain(client) {
+  const chainId = await client.getChainId();
+  const currentChain = defineChain({
+    id: chainId,
+    name: "Custom Chain",
+    nativeCurrency: {
+      name: "Ether",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    rpcUrls: {
+      default: {
+        http: ["http://localhost:8545"],
+      },
+    },
+  });
+  return currentChain;
 }
 
 connectBtn.onclick = connect;
